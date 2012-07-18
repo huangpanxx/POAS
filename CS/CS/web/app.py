eb7724ba3c71714b2b97ad99424e8932aa6ac403
snail <snail@snail-Aspire-S3.(none)>
2012-07-18 21:18:29 +0800

    在Scrapyd服务器添加了Webservice的扩展

---
+++ CS/CS/web/app.py
@@ -0,0 +1,58 @@
+'''
+Created on 2012-7-18
+
+@author: snail
+'''
+from twisted.application.service import Application
+from twisted.application.internet import TimerService, TCPServer #@UnresolvedImport
+from twisted.web import server
+from twisted.python import log
+
+from scrapyd.interfaces import IEggStorage, IPoller, ISpiderScheduler, IEnvironment
+from scrapyd.launcher import Launcher
+from scrapyd.eggstorage import FilesystemEggStorage
+from scrapyd.scheduler import SpiderScheduler
+from scrapyd.poller import QueuePoller
+from scrapyd.environ import Environment
+from scrapyd.website import Root
+from scrapyd.config import Config
+from scrapy.utils.misc import load_object
+
+def configRoot(root,config):
+    config = Config()
+    _services = config.get('services', {})
+    services = eval(_services)
+    for key,value in services.items():
+        service = load_object(value)
+        root.putChild(key, service(root))
+        
+    root.update_projects()
+    return root
+
+def application(config):
+    app = Application("Scrapyd")
+    http_port = config.getint('http_port', 6800)
+
+    poller = QueuePoller(config)
+    eggstorage = FilesystemEggStorage(config)
+    scheduler = SpiderScheduler(config)
+    environment = Environment(config)
+
+    app.setComponent(IPoller, poller)
+    app.setComponent(IEggStorage, eggstorage)
+    app.setComponent(ISpiderScheduler, scheduler)
+    app.setComponent(IEnvironment, environment)
+
+    launcher = Launcher(config, app)
+    timer = TimerService(5, poller.poll)
+    root = Root(config,app)
+    root = configRoot(root,config)
+    
+    webservice = TCPServer(http_port, server.Site(root))
+    log.msg("Scrapyd web console available at http://localhost:%s/" % http_port)
+
+    launcher.setServiceParent(app)
+    timer.setServiceParent(app)
+    webservice.setServiceParent(app)
+
+    return app
--- CS/CS/web/webservice.py
+++ CS/CS/web/webservice.py
@@ -1,12 +1,16 @@
+#coding:utf8
 '''
 Created on 2012-7-18
 
 @author: snail
 '''
-from scrapy.webservice import JsonRpcResource
-from crawl.cache import cache #@UnusedImport
 
-class CacheResource(JsonRpcResource):
+    
+from crawl.cache import cache
+from scrapyd.webservice import WsResource
+from scrapy.conf import settings
+
+class CacheResource(WsResource):
     ws_name = 'cache'
     def render_GET(self,txrequest):
         try:
@@ -30,5 +34,22 @@ class CacheResource(JsonRpcResource):
             return {'error':'param is missing'}
         except Exception:
             return {'error':'unknown'}
-                
+        
+class ItemResource(WsResource):
+    ws_name = 'item'
+    page_dir = settings['PAGE_DIRECTORY']
+    def render_GET(self,txrequest):
+        try:
+            uuid = txrequest.args['uuid'][0]
+            path = r'%s/%s' % (self.page_dir,uuid) #攻击危险,检查uuid合法性（数字+字母)
+            f = open(path)
+            data = f.read()
+            return {'content' : data}
+        except KeyError:
+            return {'error':'param uuid is needed'}
+        except IOError:
+            return {'error':'file not exist'}
+        except Exception,e: #@UnusedVariable
+            return {'error':'unknow'}
+
             
