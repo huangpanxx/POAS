943fca3d9c18af29466961fb57662044214346eb
snail <snail@snail-Aspire-S3.(none)>
2012-07-19 15:05:12 +0800

    修改包名	CSAdmin->csadmin

--- SS/SS/csadmin/__init__.py
+++ SS/SS/csadmin/__init__.py
@@ -3,6 +3,6 @@ from django.conf.urls.defaults import patterns, url
 from . import views
 
 urls = patterns('',
-                url(r'^Address/$',views.Address),
-                url(r'^Cache/$',views.Cache),
+                url(r'^address/$',views.Address),
+                url(r'^cache/$',views.Cache),
                 )
--- SS/SS/settings.py
+++ SS/SS/settings.py
@@ -124,7 +124,7 @@ INSTALLED_APPS = (
     # 'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
-    'SS.CSAdmin'
+    'SS.csadmin'
 )
 
 # A sample logging configuration. The only tangible logging
