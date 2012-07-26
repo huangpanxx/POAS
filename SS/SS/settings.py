f18c5037b247827018ffa7c5e55dbf8d7ead716b
longruiliu <longruiliu@gmail.com>
2012-07-26 12:42:18 +0000

    settings file after configure grappelli

--- SS/SS/settings.py
+++ SS/SS/settings.py
@@ -123,7 +123,8 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     # Uncomment the next line to enable the admin:
-     'django.contrib.admin',
+    'grappelli',
+    'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
     'SS.csadmin',
