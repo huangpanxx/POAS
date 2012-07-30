4b17c00a882fe18036b46498a4d87c64dfb7a059
longruiliu <longruiliu@gmail.com>
2012-07-30 16:57:37 +0000

    hot_topic finished basely,without dealing with all data

--- SS/SS/hot/views.py
+++ SS/SS/hot/views.py
@@ -5,7 +5,12 @@ from django.template.context import Context, RequestContext
 from django.shortcuts import render_to_response
 
 def hot(request,word_size = 10):
-    words = Lexical.objects.raw('SELECT * FROM hot_lexical group by total_weight desc limit %s',word_size)
+    field = request.POST.get('field', '')
+    type = request.POST.get('type', '')
+    if type == "Today":
+        words = Lexical.objects.raw('SELECT * FROM Lex where field = %s and date = now() group by total_weight desc limit %s',field,word_size)
+    else:
+        words = Lexical.objects.raw('SELECT * FROM Lex where field = %s and group by total_weight desc limit %s',field,word_size)
     return render_to_response('hot/hot.html',
                               {'words':words,
                                },
