8b201f388cef778b9fab50dc07934cdcd32fc96a
snail <snail@snail-Aspire-S3.(none)>
2012-08-09 19:23:17 +0800

    添加ｗａｐ网页

--- SS/SS/wap/__init__.py
+++ SS/SS/wap/__init__.py
@@ -1,5 +1,11 @@
+#coding:utf8
 from django.conf.urls.defaults import patterns
 from wap import views
 urls = patterns('',
-                (r'^login/$', views.login_view),
+                (r'^login/$', views.login_view), #登录
+                (r'^help/$', views.help_view), #帮助
+                (r'^transport/$',views.transport_view), #传播分析
+                (r'^hot/$',views.hot_view), #热点分析
+                (r'^topic/$',views.topic_view), #专题检测
+                (r'^source/$',views.source_view) #来源分析
                  )
--- SS/SS/wap/views.py
+++ SS/SS/wap/views.py
@@ -1,6 +1,8 @@
 # Create your views here.
 from django.contrib.auth import authenticate, logout, login
 from django.http import HttpResponse
+from django.shortcuts import render_to_response
+from django.template.context import RequestContext
 
 
 def login_view(request):
@@ -16,4 +18,24 @@ def login_view(request):
         return HttpResponse('success')
     else:
         return HttpResponse('failed')
+
+def help_view(request):
+    return render_to_response('help.html', { },
+        context_instance=RequestContext(request))
+
+
+def transport_view(request):
+    return HttpResponse('Not impliment')
+
+
+def hot_view(request):
+    return HttpResponse('Not impliment')
+
+
+def topic_view(request):
+    return HttpResponse('Not impliment')
+
+def source_view(request):
+    return HttpResponse('Not impliment')
+
 
