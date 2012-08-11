40cd0fa6051050cf7b71b929c44efbc2943fd167
snail <snail@snail-Aspire-S3.(none)>
2012-08-12 03:34:25 +0800

    修改各种跳转 美化站内信

--- SS/SS/wap/views.py
+++ SS/SS/wap/views.py
@@ -1,8 +1,12 @@
 # Create your views here.
 from django.contrib.auth import authenticate, logout, login
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render_to_response
 from django.template.context import RequestContext
+
+def render(html_path, request, dic_data):
+    return render_to_response(html_path, dic_data,
+        context_instance=RequestContext(request))
 
 
 def login_view(request):
@@ -20,26 +24,24 @@ def login_view(request):
         return HttpResponse('failed')
 
 def help_view(request):
-    return render_to_response('help.html', { },
-        context_instance=RequestContext(request))
-
+    return render('wap/help.html', request, {})
 
 def transport_view(request):
-    return HttpResponse('Not impliment')
+    return render('wap/transport.html', request, {})
 
 
 def hot_view(request):
-    return HttpResponse('Not impliment')
+    return render('wap/hot.html', request, {})
 
 
 def topic_view(request):
-    return HttpResponse('Not impliment')
+    return render('wap/topic.html', request, {})
 
 def source_view(request):
-    return HttpResponse('Not impliment')
+    return render('wap/source.html', request, {})
 
 
 def home_view(request):
-    return HttpResponse('Not impliment')
+    return HttpResponseRedirect('./hot/')
 
 
